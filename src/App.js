bf0234da5ead1162cbfec80f1f6809b4364cd87f
DerickLFAndrade <derickspy15@gmail.com>
2021-10-29 03:01:19 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import "./App.css"
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const handleClickLogin = (values) => console.log(values);
+    const validadtionLogin = yup.object().shape({
+        email: yup
+        .string()
+        .email("Não é um email valido!")
+        .required("Este campo é obrigatório!"),
+        password: yup
+        .string()
+        .min(8, "A senha deve conter no mínino 8 caracteres")
+        .required("Este campo é obrigatório!"),
+    });
+
+    const handleClickRegister = (values) => console.log (values);
+    const validadtionRegister = yup.object().shape({
+        email: yup
+            .string()
+            .email("Não é um email valido!")
+            .required("Este campo é obrigatório!"),
+        password: yup
+            .string()
+            .min(8, "A senha deve conter no mínino 8 caracteres")
+            .required("Este campo é obrigatório!"),
+
+        confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "As senhas não são iguais!"),
+    });
+
+    return (
+        <>
+            
+            <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validadtionLogin} >
+               
+               
+                <section className="login-cont">
+                    <h1>Login</h1>
+                    <Form className="login-form">
+                        
+                        <div className="login-form-group">
+                            <Field name="email" className="form-field-email" placeholder="Email" />
+                            <ErrorMessage component="span" name="email" className="form-error" />
+                        </div>
+                        <div className="login-form-group" >
+                            <Field name="password" className="form-field" placeHolder="Senha" />
+                            <ErrorMessage component="span" name="password" className="form-error" />
+                        </div>
+                        <button className="button"  type="submit">Login</button>
+                    </Form>
+                </section>
+
+            </Formik>
+
+
+            
+            <Formik initialValues={{}} onSubmit={handleClickRegister} validationSchema={validadtionRegister} >
+
+
+                <section className="login-cont" >
+                    <h1>Cadastro</h1>
+                    <Form className="login-form">
+                       
+                        <div className="login-form-group">
+                            <Field name="email" className="form-field-email" placeholder="Email" />
+                            <ErrorMessage component="span" name="email" className="form-error" />
+                        </div>
+                        <div className="login-form-group">
+                            <Field name="password" className="form-field" placeHolder="Senha" />
+                            <ErrorMessage component="span" name="password" className="form-error" />
+                        </div>
+                        <div className="login-form-group">
+                            <Field name="confirmPassword" className="form-field" placeHolder="Confirmar Senha" />
+                            <ErrorMessage component="span" name="confirmPassword" className="form-error" />
+                        </div>
+                        <button className="button" type="submit">Cadastrar</button>
+             
+
+                </Form>
+
+                </section>
+
+            </Formik>
+            
+
+
+        </>
+    )
+};
 
 export default App;
